1cfa8f67629b6e941c5452b3b576e92a69eb8a17
shibayamasho <men.in.boss2@gmail.com>
2021-02-12 21:52:53 +0900

    空のコメント送信時のバグを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      render prototype_path(@comment.prototype)
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
